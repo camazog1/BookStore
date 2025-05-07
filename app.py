785590c35c5722993ab0ea27af32041a7a51e451
camazog1 <camazog1@eafit.edu.co>
2025-05-07 17:27:14 -0500

    Update Connetion with Database

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
+from config import Config
 from extensions import db, login_manager
 from models.user import User
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstore.db'
+app.config.from_object(Config)
 
 db.init_app(app)
 login_manager.init_app(app)
@@ -14,7 +14,6 @@ login_manager.login_view = 'auth.login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Luego importar blueprints
 from controllers.auth_controller import auth
 from controllers.book_controller import book
 from controllers.purchase_controller import purchase
@@ -22,7 +21,6 @@ from controllers.payment_controller import payment
 from controllers.delivery_controller import delivery
 from controllers.admin_controller import admin
 
-# Registrar blueprints
 app.register_blueprint(auth)
 app.register_blueprint(book, url_prefix='/book')
 app.register_blueprint(purchase)
@@ -49,8 +47,6 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-# OJO este conexto crea las tablas e inicia los proveedores de entrega, 
-# se debe ejecutar cada que se reinstala y ejecuta la aplicación Bookstore
     with app.app_context():
         db.create_all()
         initialize_delivery_providers()
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secretkey')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
-SECRET_KEY = 'secretkey'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
