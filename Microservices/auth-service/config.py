2f1fd5314a62f5089a169f1de36d20e6a6c5cd0a
camazog1 <carlosm@HPPavilion.>
2025-05-11 13:10:42 -0500

    Access

--- Microservices/auth-service/config.py
+++ Microservices/auth-service/config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1045489836@host.docker.internal:3307/bookstore'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://XXXXX:XXXXX@bookstore-db.cf1fxdnidxor.us-east-1.rds.amazonaws.com:3306/bookstore'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'jwt-secret'
